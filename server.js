bf34457648b125fab5e65f890345810b4ca857e3
Victor <victortapiaegana@gmail.com>
2024-05-21 11:49:42 -0400

    ADD:imagen principal

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ import express from 'express';
 import Jimp from 'jimp';
 import { v4 as uuid } from 'uuid';
 import path from 'path';
+import { create } from 'express-handlebars';
 
 const app = express();
+
+const hbs = create({ 
+                   extname: '.hbs',   
+                   layoutsDir:path.join(process.cwd(),'/views') ,
+                   partialsDir:path.join(process.cwd(),'/views/componentes') ,
+                   defaultLayout:false
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.use('/assets',express.static(path.join(process.cwd(),'public')));
 
@@ -22,7 +34,6 @@ app.get('/procesarimagen',async (req,res)=>{
         nombreImagen 
         ? ImagenATransformar = nombreImagen 
         : ImagenATransformar = selectImagenes;
-        
 
             const nombreNuevaImagen = `${uuid().slice(0,8)}.jpeg`;
 
@@ -31,32 +42,15 @@ app.get('/procesarimagen',async (req,res)=>{
             const imagen = await Jimp.read( ImagenATransformar )
             
               await imagen         
-                .grayscale()
-                .quality(100)
-                .resize(350,Jimp.AUTO)
-                .writeAsync( NuevaImagen )
+                   .grayscale()
+                   .quality(100)
+                   .resize(350,Jimp.AUTO)
+                   .writeAsync( NuevaImagen )
 
-            res.send(
-                     `<body style="background-color: #212529;color:#6c757d;">            
-                        <h1 style="text-align:center;">Resultado imagenes Procesadas</h1>
-
-                        <section style="display:flex; justify-content: center; gap:50px">
-
-                            <div style="width:600px;">
-                              <h2>Original</h2>
-                                <img src="${ ImagenATransformar }" style="max-width: 100%; height: auto;" alt="Imagen Original">
-                            </div>
-                            
-                            <div style="width: 600px;display:flex;flex:0;flex-direction: column;">
-                              <h2>Modificada</h2>
-                                <img src="/assets/imagenes/${nombreNuevaImagen}" alt="Imagen en escala de grises">
-                                <strong>Nombre del Arhivo: ${nombreNuevaImagen}</strong>
-                            </div>
-
-                        </section>
-
-                      </body>`
-            );
+                res.render('procesarimagen.hbs',{
+                     imagenOriginal:ImagenATransformar,
+                     ImagenTransformada:nombreNuevaImagen
+                })
     }else{
 
           res.status(400).send('Datos incompletos')
